2bc06f69a429c16bec2e4904ae77c8f4efc33763
yeon2731 <kite2731@gmail.com>
2025-10-19 17:33:35 +0900

    feat(calculator): add input/output structure

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        
+        int[] number_arr = parseString(input);
+        int sum = 0;
+        for (int i = 0; i < number_arr.length; i++) {
+            sum += number_arr[i];
+        }
+
+        System.out.println("결과 : " + sum);
+    }
+
+    public static int[] parseString(String input) {
+        return new int[0];
     }
 }
